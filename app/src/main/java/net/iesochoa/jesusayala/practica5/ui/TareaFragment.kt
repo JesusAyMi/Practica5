22147700787548b8452bd978af5201bfd401e337
JesusAyala <jesusayalamilan2@gmail.com>
2023-03-04 19:52:24 +0100

    Punto 40

--- app/src/main/java/net/iesochoa/jesusayala/practica5/ui/TareaFragment.kt
+++ app/src/main/java/net/iesochoa/jesusayala/practica5/ui/TareaFragment.kt
@@ -1,11 +1,15 @@
 package net.iesochoa.jesusayala.practica5.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import net.iesochoa.jesusayala.practica5.R
 import net.iesochoa.jesusayala.practica5.databinding.FragmentTareaBinding
 
@@ -33,6 +37,9 @@ class TareaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        iniciaSpCategoria()
+        iniciaSpPrioridad()
+
         /*binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }*/
@@ -41,5 +48,66 @@ class TareaFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun iniciaSpCategoria() {
+        ArrayAdapter.createFromResource(
+            //contexto suele ser la Activity
+            requireContext(),
+            //array de strings
+            R.array.categoria,
+            //layout para mostrar el elemento seleccionado
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Layout para mostrar la apariencia de la lista
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // asignamos el adaptador al spinner
+            binding.spCategoria.adapter = adapter
+            binding.spCategoria.onItemSelectedListener=object : AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(p0: AdapterView<*>?, v: View?, posicion: Int, id: Long) {
+                    //recuperamos el valor
+                    val valor=binding.spCategoria.getItemAtPosition(posicion)
+                    //creamos el mensaje desde el recurso string parametrizado
+                    val mensaje=getString(R.string.mensaje_categoria,valor)
+                    //mostramos el mensaje donde "binding.root" es el ContrainLayout principal
+                    Snackbar.make(binding.root, mensaje, Snackbar.LENGTH_LONG).setAction("Action", null).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+            }
+        }
+    }
+
+    private fun iniciaSpPrioridad() {
+        ArrayAdapter.createFromResource(
+            //contexto suele ser la Activity
+            requireContext(),
+            //array de strings
+            R.array.prioridad,
+            //layout para mostrar el elemento seleccionado
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Layout para mostrar la apariencia de la lista
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // asignamos el adaptador al spinner
+            binding.spPrioridad.adapter = adapter
+            binding.spPrioridad.onItemSelectedListener=object : AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(p0: AdapterView<*>?, v: View?, posicion: Int, id: Long) {
+                    //el array son 3 elementos y "alta" ocupa la tercera posición
+                    if(posicion==2){
+                        binding.clyTarea.setBackgroundColor(requireContext().getColor(R.color.prioridad_alta))
+                    }else {//si no es prioridad alta quitamos el color
+                        binding.clyTarea.setBackgroundColor(Color.TRANSPARENT)
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    binding.clyTarea.setBackgroundColor(Color.TRANSPARENT)
+                }
+            }
+        }
     }
 }
